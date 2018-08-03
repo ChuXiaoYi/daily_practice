a24e0827cf792326f03cbf08e7a6ccc1628b039c
ChuXiaoYi <a895706056@163.com>
2018-08-03 20:33:34 +0800

    mysite

---
+++ django_study/MySite/Post/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------
+#       @Time    : 2018/8/3 下午5:29
+#       @Author  : cxy =.= 
+#       @File    : urls.py
+#       @Software: PyCharm
+# --------------------------------------
+from django.conf.urls import url
+from . import views
+app_name = 'Post'
+
+urlpatterns = [
+
+]
